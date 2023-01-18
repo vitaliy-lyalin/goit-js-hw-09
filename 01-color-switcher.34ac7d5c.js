1e5e210270816b53473a10f9c1c9b23d967156eb
vitaliy-lyalin <vitaliy.lyalin@gmail.com>
2023-01-18 19:00:05 +0000

    Deploying to gh-pages from @ vitaliy-lyalin/goit-js-hw-09@82e84d2c9715c527710e8bc75239ded64a122770 🚀

---
+++ 01-color-switcher.34ac7d5c.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=null;function o(){return"#".concat(Math.floor(16777215*Math.random()).toString(16))}t.addEventListener("click",(function(){document.body.style.backgroundColor=o(),t.setAttribute("disabled","true"),n=setInterval((function(){document.body.style.backgroundColor=o()}),1e3)})),e.addEventListener("click",(function(){clearInterval(n),t.removeAttribute("disabled")}))}();
+//# sourceMappingURL=01-color-switcher.34ac7d5c.js.map
