82e84d2c9715c527710e8bc75239ded64a122770
Vitaliy Lyalin <vitaliy.lyalin@gmail.com>
2023-01-18 20:59:41 +0200

    Add script JS for 01-color-switcher.

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+let timerId = null;
+
+startBtn.addEventListener('click', () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  startBtn.setAttribute('disabled', 'true');
+
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  startBtn.removeAttribute('disabled');
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
